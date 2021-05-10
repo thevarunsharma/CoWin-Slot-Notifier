52f394d5df660a0b397fecc2d72f9deb48621e92
thevarunsharma <varunsharma123.del@gmail.com>
2021-05-10 23:10:55 +0530

    split notifier into main and utils files

---
+++ cowin_notifier/__init__.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import click
+import gc
+import json
+import time
+from .cowin_notify_utils import get_available_slots, send_email_notif, get_diff_value
+from diskcache import Cache
+
+RECUR_PERIOD = 60 * 5          # 5 minutes
+
+@click.command()
+@click.argument('date')
+@click.option('--verbose', '-v', is_flag=True, help='Display verbose information')
+@click.option('--pincode', '-p', default=None, help='Pincode to search by')
+@click.option('--district', '-d', default=None, help='District to search by')
+@click.option('--state', '-s', default=None, help='State for district to search by')
+@click.option('--age-group', '-a', default=45, help='Age Group to filter by')
+def main(date,
+         pincode,
+         district,
+         state,
+         age_group,
+         verbose):
+    """
+    Sends E-Mail Notification for available CoWin Slots on DATE
+    """
+    cache = Cache(".previous")
+    cache.expire()
+    
+    try:
+        while True:
+            # fetch available slots
+            available = get_available_slots(date, 
+                                            pincode=pincode, 
+                                            state=state, 
+                                            district=district, 
+                                            age_group=age_group)
+            
+            # get difference from cached value and update it
+            centers = get_diff_value(available,
+                                     cache,
+                                     date,
+                                     pincode=pincode,
+                                     state=state,
+                                     district=district,
+                                     age_group=age_group)
+            
+            if centers is not None:
+                # if cached value doesn't match current value
+                click.echo("{0:-^100}".format("NEW CoWIN SLOTS FOUND"))
+                available['centers'] = centers
+
+                if verbose:
+                    click.echo(json.dumps(available, indent=2))
+    
+                if available['centers']:
+                    send_email_notif(available)
+                    click.echo("EMAIL NOTIFICATION SENT SUCCESSFULLY!")
+            
+            gc.collect()
+            time.sleep(RECUR_PERIOD)
+    
+    except KeyboardInterrupt:
+        cache.close()
+        click.echo("Aborted.")
+
+if __name__ == '__main__':
+    main()
