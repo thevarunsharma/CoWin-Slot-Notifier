1ddc9d8881207d1ea7b1f75b1200982e65ee7bb2
thevarunsharma <varunsharma123.del@gmail.com>
2021-05-11 21:08:05 +0530

    added prompt based sender credential input

--- cowin_notifier/__init__.py
+++ cowin_notifier/__init__.py
@@ -4,32 +4,44 @@ import click
 import gc
 import json
 import time
-from .cowin_notify_utils import get_available_slots, send_email_notif, get_diff_value
+from .cowin_data_utils import get_ist_date, get_available_slots, get_diff_value
+from .cowin_mail_utils import CowinMailer
 from diskcache import Cache
 
 RECUR_PERIOD = 60 * 5          # 5 minutes
 
 @click.command()
-@click.argument('date')
 @click.option('--verbose', '-v', is_flag=True, help='Display verbose information')
 @click.option('--pincode', '-p', default=None, help='Pincode to search by')
 @click.option('--district', '-d', default=None, help='District to search by')
 @click.option('--state', '-s', default=None, help='State for district to search by')
 @click.option('--age-group', '-a', default=45, help='Age Group to filter by')
-def main(date,
-         pincode,
+def main(pincode,
          district,
          state,
          age_group,
          verbose):
     """
-    Sends E-Mail Notification for available CoWin Slots on DATE
+    Sends E-Mail Notification for available CoWin Slots
     """
     cache = Cache(".previous")
-    cache.expire()
+    # sender's name for email
+    sender_name = click.prompt("Sender's Name", default="CoWin Notifier")
+    # sender's email address
+    sender_email = click.prompt("Sender's Email")
+    # sender's login password
+    password = click.prompt("Password", hide_input=True, confirmation_prompt=True)
+    
+    click.echo("CoWin Notifier running...")
+    # mailer for sending emails
+    mailer = CowinMailer(sender_email, 
+                         sender_name, 
+                         password)
     
     try:
         while True:
+            cache.expire()
+            date = get_ist_date()
             # fetch available slots
             available = get_available_slots(date, 
                                             pincode=pincode, 
@@ -55,7 +67,7 @@ def main(date,
                     click.echo(json.dumps(available, indent=2))
     
                 if available['centers']:
-                    send_email_notif(available)
+                    mailer.send_email_notif(available)
                     click.echo("EMAIL NOTIFICATION SENT SUCCESSFULLY!")
             
             gc.collect()
